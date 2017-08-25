1ed727882c96bce19f870ffa3df24718f1658941
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-08-25 10:23:52 -0700

    add forms to take user inputs

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//Business Logic
+function Pizza(size){
+	this.size = size;
+	this.toppings =[];
+}
+Pizza.prototype.cost = function(){
+	if(this.size = small){
+
+	}
+}
+
+
+//User Interface Logic
+$(document).ready(function(){
+
+});
